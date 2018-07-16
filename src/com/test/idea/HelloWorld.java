a5eedf6ae92410280cf424ac95bc90526c90442f
程艳明 <2035195737@qq.com>
2018-07-17 00:23:19 +0800

    add a method

--- src/com/test/idea/HelloWorld.java
+++ src/com/test/idea/HelloWorld.java
@@ -4,6 +4,9 @@ package com.test.idea;
  * created by Anna on 2018/7/16
  */
 public class HelloWorld {
+    public String say(String name){
+        return name;
+    }
     public static void main(String[] args){
         System.out.println("helo world");
     }
