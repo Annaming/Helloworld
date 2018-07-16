bad4b1b20fb26bb61cdf6009317967c06bd0bfe2
程艳明 <2035195737@qq.com>
2018-07-17 00:13:54 +0800

    Initial commit

---
+++ src/com/test/idea/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test.idea;
+
+/**
+ * created by Anna on 2018/7/16
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("helo world");
+    }
+}
